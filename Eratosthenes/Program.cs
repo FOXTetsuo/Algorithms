e39ee4453e2006e07bdcfb7209221e41f5a30034
FOXTetsuo <pownur@gmail.com>
2023-09-07 22:07:48 +0200

    initial commit, EratosThenes

---
+++ Eratosthenes/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int maxNumber = 30;
+bool[] isPrime = new bool[maxNumber + 1];
+
+// make array of numbers
+for (int i = 2; i <= maxNumber; i++)
+{
+    isPrime[i] = true;
+}
+
+// sieve that stuff
+for (int i = 2; i * i <= maxNumber; i++)
+{
+    if (isPrime[i])
+    {
+        for (int k = i * i; k <= maxNumber; k += i)
+        {
+            isPrime[k] = false;
+        }
+    }
+}
+
+// print
+for (int i = 2; i <= maxNumber; i++)
+{
+    if (isPrime[i])
+    {
+        Console.WriteLine(i);
+    }
+}
